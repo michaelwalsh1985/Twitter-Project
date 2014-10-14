c65647a421b6f8c164e7685e3f268ca0abd78bb7
PhilipGough <philip.p.gough@gmail.com>
2014-10-14 14:38:17 +0100

    Fixed db problem-self inflicted

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141009193133) do
+ActiveRecord::Schema.define(version: 20141009200126) do
 
-# Could not dump table "users" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
