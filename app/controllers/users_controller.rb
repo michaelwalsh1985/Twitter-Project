4a4b9a104998865e0a76e5380561ea30f2b4d52a
PhilipGough <philip.p.gough@gmail.com>
2014-10-14 18:49:03 +0100

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,20 @@ class UsersController < ApplicationController
   end
 
   def new
+  	  @user = User.new
   end
+
+       def create
+        secure_params = params.require(:user).permit(:name, :email, 
+                                  :password, :password_confirmation)
+        @user = User.new(secure_params)
+        if @user.save
+           # Handle a successful save.
+           flash[:success] = "Welcome to the Twitter App!"
+           redirect_to @user
+        else
+           render 'new'    
+        end
+      end
   
 end
