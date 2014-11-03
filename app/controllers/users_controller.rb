f904b1feb02f064b3216ad55a916ac31500a5f77
PhilipGough <philip.p.gough@gmail.com>
2014-11-03 21:54:00 +0000

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
         @user = User.new(secure_params)
         if @user.save
            # Handle a successful save.
+           sign_in @user
            flash[:success] = "Welcome to the Twitter App!"
            redirect_to @user
         else
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
 
-  resources :users       # NEW LINE
+  resources :users      
+  resources :sessions, only: [:new, :create, :destroy]
 
  
 
@@ -10,6 +11,8 @@ Rails.application.routes.draw do
   match '/about',   to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/signup',  to: 'users#new', via: 'get'
+  match '/signin',  to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: :delete
   match '/help',    to: 'static_pages#help' , via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
